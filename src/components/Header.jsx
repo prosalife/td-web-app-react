b1919cd1e0e5f5d26eff333466efc3f5ea920631
prosalife <66415757+prosalife@users.noreply.github.com>
2022-10-12 08:06:02 +0300

    12-10022 8:06

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Button from "../Button/Button"
 import { useTelegram } from 'src/hooks/useTelegram'
+import './Header.css'
+
 
 const Header = () => {
     const { user, onClose } = useTelegram()
